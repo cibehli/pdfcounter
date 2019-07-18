c670caf9593d7f138fa90e2d686c2f1eeb11f973
cibehli <cibehli@live.com.mx>
2019-07-18 15:41:19 -0600

    pdf counter

--- pdfcounter/RadForm1.Designer.cs
+++ pdfcounter/RadForm1.Designer.cs
@@ -28,20 +28,95 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.lbl_paginas = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
 			this.SuspendLayout();
 			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(13, 13);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(240, 315);
+			this.treeView1.TabIndex = 0;
+			this.treeView1.Click += new System.EventHandler(this.treeView1_Click);
+			this.treeView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseMove);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(272, 13);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(432, 315);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(73, 335);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Seleccionar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// lbl_paginas
+			// 
+			this.lbl_paginas.AutoSize = true;
+			this.lbl_paginas.Location = new System.Drawing.Point(628, 335);
+			this.lbl_paginas.Name = "lbl_paginas";
+			this.lbl_paginas.Size = new System.Drawing.Size(0, 13);
+			this.lbl_paginas.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(544, 335);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(78, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Total Paginas:";
+			// 
 			// RadForm1
-			//
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(809, 422);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lbl_paginas);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.treeView1);
 			this.Name = "RadForm1";
+			// 
+			// 
+			// 
+			this.RootElement.ApplyShapeToControl = true;
 			this.Text = "RadForm1";
+			this.Load += new System.EventHandler(this.RadForm1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.ToolTip toolTip1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label lbl_paginas;
+		private System.Windows.Forms.Label label2;
 	}
 }
--- pdfcounter/RadForm1.cs
+++ pdfcounter/RadForm1.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Telerik.WinControls;
 using Telerik.WinControls.UI;
+using iTextSharp.text.xml;
+using iTextSharp.text.pdf;
+
 
 namespace pdfcounter
 {
@@ -17,5 +21,181 @@ namespace pdfcounter
 		{
 			InitializeComponent();
 		}
+
+		private void RadForm1_Load(object sender, EventArgs e)
+		{
+			string dir= @"C:\COMPARTIDO\Cobranza\";
+			LoadDirectory(dir);
+		}
+		private void LlenarGrid()
+		{
+
+		}
+		public void LoadDirectory(string Dir)
+		{
+			DirectoryInfo di = new DirectoryInfo(Dir);
+			//Setting ProgressBar Maximum Value  
+			TreeNode tds = treeView1.Nodes.Add(di.Name);
+			tds.Tag = di.FullName;
+			//tds.StateImageIndex = 0;
+			//tds.StateImageIndex = 6;
+			//tds.ImageIndex = 6;
+			//LoadFiles(Dir, tds);
+			DirectoryInfo dir = new DirectoryInfo(Dir);
+			
+			LoadSubDirectories(dir, tds);
+			
+		}
+	
+		private void LoadSubDirectories(DirectoryInfo dir, TreeNode td)
+		{
+			// Get all subdirectories  
+			DirectoryInfo[] subdirectoryEntries = dir.GetDirectories();
+			// Loop through them to see if they have any other subdirectories  
+			int driveImage;
+			foreach (DirectoryInfo subdirectory in subdirectoryEntries)
+			{
+				DriveInfo dr = new DriveInfo(subdirectory.FullName);
+				switch (dr.DriveType)    //set the drive's icon
+				{
+					case DriveType.CDRom:
+						driveImage = 3;
+						break;
+					case DriveType.Network:
+						driveImage = 6;
+						break;
+					case DriveType.NoRootDirectory:
+						driveImage = 8;
+						break;
+					case DriveType.Unknown:
+						driveImage = 8;
+						break;
+					default:
+						driveImage = 2;
+						break;
+				}
+				
+				DirectoryInfo di = new DirectoryInfo(subdirectory.FullName);
+				TreeNode tds = td.Nodes.Add(di.Name, di.Name, driveImage);
+				
+				
+				
+				tds.ImageIndex = driveImage;
+				tds.SelectedImageIndex = driveImage;
+				tds.ImageKey = driveImage.ToString();
+				tds.SelectedImageKey = driveImage.ToString();
+				tds.StateImageIndex = driveImage;
+				tds.Tag = di.FullName;
+				//LoadFiles(subdirectory, tds);
+				LoadSubDirectories(subdirectory, tds);
+
+			}
+		}
+		private void LoadFiles(string dir, TreeNode td)
+		{
+			string[] Files = Directory.GetFiles(dir, "*.*");
+
+			// Loop through them to see files  
+			foreach (string file in Files)
+			{
+				FileInfo fi = new FileInfo(file);
+				TreeNode tds = td.Nodes.Add(fi.Name);
+				tds.Tag = fi.FullName;
+				
+			}
+		}
+
+		private void treeView1_MouseMove(object sender, MouseEventArgs e)
+		{
+			// Get the node at the current mouse pointer location.  
+			TreeNode theNode = this.treeView1.GetNodeAt(e.X, e.Y);
+
+			// Set a ToolTip only if the mouse pointer is actually paused on a node.  
+			if (theNode != null && theNode.Tag != null)
+			{
+				// Change the ToolTip only if the pointer moved to a new node.  
+				if (theNode.Tag.ToString() != this.toolTip1.GetToolTip(this.treeView1))
+					this.toolTip1.SetToolTip(this.treeView1, theNode.Tag.ToString());
+
+			}
+			else     // Pointer is not over a node so clear the ToolTip.  
+			{
+				this.toolTip1.SetToolTip(this.treeView1, "");
+			}
+		}
+
+		private void treeView1_Click(object sender, EventArgs e)
+		{
+			
+			
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			dataGridView1.DataSource = null;
+			dataGridView1.Update();
+			lbl_paginas.Text = "";
+			TreeNode tree = new TreeNode();
+			tree = treeView1.SelectedNode;
+			DirectoryInfo df = new DirectoryInfo(@"C:\COMPARTIDO\" + tree.FullPath);
+			DirectoryInfo[] fo = df.GetDirectories();
+			FileInfo[] archivos=null;
+			FileInfo[] fi;
+			List<FileInfo> filepdf= new List<FileInfo>();
+			if (fo.Count()>0)
+			{
+				foreach(DirectoryInfo dy in fo)
+				{
+					foreach(FileInfo inf in dy.GetFiles("*.pdf*"))
+					{
+						filepdf.Add(inf);
+					}
+					
+				}
+				archivos = filepdf.ToArray();
+			}
+			else
+			{
+				archivos = df.GetFiles("*.pdf*");
+				
+			}
+			fi = archivos;
+			string path = "";
+			int numberOfPages = 0;
+			DataTable table = new DataTable();
+			DataColumn fNameColumn = new DataColumn();
+			fNameColumn.DataType = System.Type.GetType("System.String");
+			fNameColumn.ColumnName = "Archivo";
+
+			table.Columns.Add(fNameColumn);
+			fNameColumn = new DataColumn();
+			fNameColumn.DataType = System.Type.GetType("System.String");
+			fNameColumn.ColumnName = "Paginas";
+			table.Columns.Add(fNameColumn);
+			foreach (FileInfo file in fi)
+			{
+				path = file.FullName;
+				PdfReader pdfReader = new PdfReader(path);
+				numberOfPages = pdfReader.NumberOfPages;
+
+				DataRow row;
+				row = table.NewRow();
+				row["Archivo"] = file.Name;
+				row["Paginas"] = numberOfPages;
+				table.Rows.Add(row);
+
+			}
+			dataGridView1.DataSource = table;
+			dataGridView1.Columns[0].Width = 250;
+			dataGridView1.Update();
+			int sumapaginas = 0;
+			foreach (DataGridViewRow row in dataGridView1.Rows)
+			{
+				sumapaginas += int.Parse(row.Cells[1].Value.ToString());
+			}
+			lbl_paginas.Text = sumapaginas.ToString();
+
+		}
 	}
+
 }
